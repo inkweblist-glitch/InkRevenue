352f4382ee8272c1ea6a69fbec4f005c73fc18e7
ARPIT TYAGI <Arpit.tyagi@stargazedmedia.com>
2026-03-16 15:17:07 +0530

    changed whatsapp number

--- src/app/Whatsapp/page.jsx
+++ src/app/Whatsapp/page.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/919311375769"
+      href="https://wa.me/919808068484"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-5 right-5 z-50 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition"
